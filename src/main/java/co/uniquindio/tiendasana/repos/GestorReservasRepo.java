e8afb3e3cd630aea885eee5860140e6a8c708367
MiguelA05 <126737880+MiguelA05@users.noreply.github.com>
2025-05-17 23:32:01 -0500

    Update GestorReservasRepo.java

--- src/main/java/co/uniquindio/tiendasana/repos/GestorReservasRepo.java
+++ src/main/java/co/uniquindio/tiendasana/repos/GestorReservasRepo.java
@@ -1,7 +1,25 @@
 package co.uniquindio.tiendasana.repos;
 
+import co.uniquindio.tiendasana.utils.CarritoConstantes;
+import co.uniquindio.tiendasana.utils.GestorReservaConstantes;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.api.services.sheets.v4.Sheets;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class GestorReservasRepo {
+    private final Sheets sheetsService;
+
+    @Value("${google.sheets.spreadsheet-id}")
+    private String spreadsheetId;
+
+    private final String SHEET_NAME = GestorReservaConstantes.HOJA_GESTOR;
+    private final String SHEET_NAME_MESA = GestorReservaConstantes.HOJA_MESA;// nombre de tu hoja en Google Sheets
+
+    private final ObjectMapper objectMapper = new ObjectMapper();
+
+    public GestorReservasRepo(Sheets sheetsService) {
+        this.sheetsService = sheetsService;
+    }
 }
