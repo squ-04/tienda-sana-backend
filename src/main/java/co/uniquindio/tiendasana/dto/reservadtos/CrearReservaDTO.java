d4ff2a5fcb1f9e6eada8b55a624972013fa3dd17
JUANMANUELUQ <126833069+JUANMANUELUQ@users.noreply.github.com>
2025-05-25 11:01:43 -0500

    Update CrearReservaDTO.java

--- src/main/java/co/uniquindio/tiendasana/dto/reservadtos/CrearReservaDTO.java
+++ src/main/java/co/uniquindio/tiendasana/dto/reservadtos/CrearReservaDTO.java
@@ -1,6 +1,6 @@
 package co.uniquindio.tiendasana.dto.reservadtos;
 
-import co.uniquindio.tiendasana.model.documents.Mesa;
+import co.uniquindio.tiendasana.dto.MesaDTO;
 
 import java.time.LocalDateTime;
 import java.util.List;
@@ -9,6 +9,6 @@ public record CrearReservaDTO(
         String emailUsuario,
         LocalDateTime fechaReserva,
         int cantidadPersonas,
-        List<Mesa> mesas
+        List<MesaDTO> mesas
 ) {
 }
